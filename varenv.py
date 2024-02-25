f71e682560a63291de51b04d018a95d99e913cb1
merrivoir <merrivoir@gmail.com>
2024-02-25 19:56:15 +0600

    printenv

--- varenv.py
+++ varenv.py
@@ -6,4 +6,4 @@ id_sec = os.getenv("api_id_sec")
 hash_sec = os.getenv("api_hash_sec")
 weather_key = os.getenv("ow_api_key") 
 
-print(f"Основной: {id_main}, хэш1: {hash_main}, Вторичный: {id_sec}, хэш2: {hash_sec}, Погода: {weather_key}")
+#print(f"Основной: {id_main}, хэш1: {hash_main}, Вторичный: {id_sec}, хэш2: {hash_sec}, Погода: {weather_key}")
